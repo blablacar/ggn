b934487843b68baca01bdd6ccc16e6e768f754b1
Arnaud Lemaire <alemaire@norad.fr>
2015-11-25 11:24:53 +0100

    use go install to install package

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -x
 set -e
 start=`date +%s`
 dir=$( dirname $0 )
@@ -12,17 +13,6 @@ ENVS="darwin\nlinux"
 # clean
 rm -Rf $dir/dist/*-amd64
 
-# binary
-#[ -f $GOPATH/bin/go-bindata ] || go get -u github.com/jteeuwen/go-bindata/...
-#mkdir -p $dir/dist/bindata
-
-#[ -f $dir/aci-bats/aci-bats.aci ] || $dir/aci-bats/build.sh
-#cp $dir/aci-bats/aci-bats.aci $dir/dist/bindata
-#[ -f $dir/dist/bindata/busybox ] || cp /bin/busybox $dir/dist/bindata/
-#[ -f $dir/dist/bindata/attributes-merger ] || wget "https://github.com/blablacar/attributes-merger/releases/download/0.1/attributes-merger" -O $dir/dist/bindata/attributes-merger
-#[ -f $dir/dist/bindata/confd ] || wget "https://github.com/kelseyhightower/confd/releases/download/v0.10.0/confd-0.10.0-linux-amd64" -O $dir/dist/bindata/confd
-#go-bindata -nomemcopy -pkg dist -o $dir/dist/bindata.go $dir/dist/bindata/...
-
 # format && test
 gofmt -w -s .
 godep go test -cover $dir/...
@@ -30,14 +20,13 @@ godep go test -cover $dir/...
 # build
 if `command -v parallel >/dev/null 2>&1`; then
     echo -e "$ENVS" | parallel --will-cite -j10 --workdir . "GOOS={} GOARCH=amd64 godep go build -o dist/{}-amd64/ggn"
-#    mv dist/windows-amd64/ggn dist/windows-amd64/ggn.exe
 else
     for e in `echo -e "$ENVS"`; do
         GOOS="$e" GOARCH=amd64 godep go build -o "dist/${e}-amd64/ggn"
     done
 fi
 
-cp $dir/dist/linux-amd64/ggn $GOPATH/bin/ggn
+godep go install
 
 end=`date +%s`
 echo "Duration : $((end-start))s"
