41732df7a8be6b2f011b1d219d7ed9d556eac27a
Arnaud Lemaire <alemaire@norad.fr>
2016-12-30 12:27:13 +0100

    fix version and commit hash

--- main.go
+++ main.go
@@ -8,11 +8,11 @@ import (
 	_ "github.com/n0rad/go-erlog/register"
 )
 
-var CommitHash string
-var Version string
+var BuildCommit string
+var BuildVersion string
 var BuildTime string
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	commands.Execute(CommitHash, Version, BuildTime)
+	commands.Execute(BuildCommit, BuildVersion, BuildTime)
 }
